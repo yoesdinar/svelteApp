c387e1f335d6729517b9656f758cc2ed054c7807
Doni Yusdinar <doni.yusdinar@gdplabs.id>
2020-07-03 10:55:55 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Navbar from "./layout/Navbar.svelte";
+  import "../node_modules/materialize-css/dist/css/materialize.min.css";
+
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from './pages/Home.svelte';
+  import About from './pages/About.svelte'; 
+</script>
+
+<style>
+
+</style>
+
+<Router>
+  <Navbar />
+  <div class="container">
+    <Route path="/" component={Home} />
+    <Route path="about" component={About} />
+  </div>
+</Router>
